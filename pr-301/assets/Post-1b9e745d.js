fcc73fe5946701c06619115b5c66f0c589dac979
MTRNord <MTRNord@users.noreply.github.com>
2023-01-18 08:39:29 +0000

    deploy: MTRNord/matrix-art@2789325cb966d7cb6e27916ec410451968485721

---
+++ pr-301/assets/Post-1b9e745d.js
@@ -0,0 +1,2 @@
+import{p as e,j as s}from"./vendor-27d39638.js";import{H as a}from"./index-4dbfac25.js";function t(){return e(),s.jsxs("div",{className:"flex flex-col",children:[s.jsx("header",{children:s.jsx(a,{})}),s.jsx("main",{className:"m-12 mt-6"})]})}export{t as default};
+//# sourceMappingURL=Post-1b9e745d.js.map
