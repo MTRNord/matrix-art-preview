4d20f7092c0d79e0754e97675541c98c56b385f4
MTRNord <MTRNord@users.noreply.github.com>
2023-01-18 09:53:01 +0000

    deploy: MTRNord/matrix-art@4073ad9f7af2c2edc846cfcc3d252f91bf00474d

---
+++ pr-301/assets/Profile-1e875d09.js
@@ -0,0 +1,2 @@
+import{p as e,j as s}from"./vendor-4f1aeb5a.js";import{H as a}from"./index-f8d8e96a.js";function i(){return e(),s.jsxs("div",{className:"flex flex-col",children:[s.jsx("header",{children:s.jsx(a,{})}),s.jsx("main",{className:"m-12 mt-6"})]})}export{i as default};
+//# sourceMappingURL=Profile-1e875d09.js.map
